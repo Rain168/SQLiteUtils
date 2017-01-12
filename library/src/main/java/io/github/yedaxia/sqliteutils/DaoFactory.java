92b9990b62ca86ae4961aa899e15d2412e98d235
Darcy <yeguozhong@chufanginc.com>
2017-01-12 11:21:18 +0800

    modify for bintray

---
+++ library/src/main/java/io/github/yedaxia/sqliteutils/DaoFactory.java
@@ -0,0 +1,21 @@
+package io.github.yedaxia.sqliteutils;
+
+
+/**
+ * use this factory to create your sqlite data access object
+ * @author Darcy yeguozhong@yeah.net
+ */
+public class DaoFactory {
+	
+	/**
+	 * call this method to new a GenericDao
+	 * @param db
+	 * @param modelClazz
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> IBaseDao<T> createGenericDao(DbSqlite db, Class<?> modelClazz){
+		return new GenericDao<T>(db,modelClazz);
+	}  
+
+}
